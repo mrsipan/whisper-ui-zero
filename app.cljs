30595df9e53c070817132498525fad873fca2ea6
bsanchez <mrsipan@users.noreply.github.com>
2026-03-31 14:42:15 -0400

    files

--- app.cljs
+++ app.cljs
@@ -1,60 +1,80 @@
-(ns demo
+(ns whisper.ui
   (:require
-   ["https://cdn.jsdelivr.net/npm/eucalypt@0.0.3/+esm" :as r]))
+   ["https://cdn.jsdelivr.net/npm/eucalypt@0.0.3/+esm" :refer [render atom]]
+            ; ["./squint/string.js" :as str]
+            ))
 
-(defn new-game []
-  {:board (vec (repeat 9 nil))
-   :x-turn? true})
+;; --- State Management ---
+(defonce App-State (atom {:recording? false
+                      :chunks []}))
 
-(defonce state (r/atom (new-game)))
+(def server-url "http://localhost:8080/inference")
 
-(defn winner [board]
-  (let [lines [[0 1 2] [3 4 5] [6 7 8]
-               [0 3 6] [1 4 7] [2 5 8]
-               [0 4 8] [2 4 6]]]
-    (some (fn [[a b c]]
-            (when (and (get board a)
-                       (= (get board a) (get board b) (get board c)))
-              (get board a)))
-          lines)))
+;; --- Audio Logic ---
+(defn send-to-server! [blob]
+  (let [form-data (js/FormData.)]
+    (.append form-data "file" blob "chunk.webm")
+    (-> (js/fetch server-url #js {:method "POST" :body form-data})
+        (.then (fn [res] (.json res)))
+        (.then (fn [data]
+                 (swap! App-State update :chunks conj (.-text data))))
+        (.catch (fn [err] (js/console.error "Upload error:" err))))))
 
-(defn square [i]
-  (let [{:keys [board x-turn?]} @state
-        mark (get board i)]
-    [:div {:style {:width "60px" :height "60px"
-                   :border "1px solid #000"
-                   :display "flex" :align-items "center"
-                   :justify-content "center"
-                   :font-size "24px"
-                   :cursor "pointer"}
-           :on-click #(when (and (nil? mark) (not (winner board)))
-                        (swap! state update :board assoc i (if x-turn? "X" "O"))
-                        (swap! state update :x-turn? not))}
-     mark]))
+(defn create-recorder [stream]
+  (let [recorder (js/MediaRecorder. stream)]
+    (set! (.-ondataavailable recorder)
+          (fn [e] (when (and (:recording? @App-State) (> (.-size (.-data e)) 0))
+                    (send-to-server! (.-data e)))))
+    recorder))
 
-(defn board-view []
-  (let [{:keys [board]} @state]
-    [:div {:style {:display "grid"
-                   :grid-template-columns "repeat(3, 60px)"
-                   :grid-template-rows "repeat(3, 60px)"
-                   :gap "2px"}}
-     (for [i (range 9)]
-       ^{:key i} [square i])]))
+(def recorders (atom []))
 
-(defn game []
-  (let [{:keys [board x-turn?]} @state
-        w (winner board)]
-    [:div {:style {:font-family "sans-serif"}}
-     [board-view]
-     (cond
-       w [:p (str "Winner: " w)]
-       (every? some? board) [:p "Draw!"]
-       :else [:p (str "Next turn: " (if x-turn? "X" "O"))])
-     [:button {:on-click #(reset! state (new-game))}
-      "Restart"]]))
+(defn stop-streaming! []
+  (swap! App-State assoc :recording? false)
+  (doseq [r @recorders] (.stop r))
+  (reset! recorders []))
 
-(r/render
-  [game]
+(defn start-streaming! []
+  (swap! App-State assoc :recording? true :chunks [])
+  (-> (.getUserMedia js/navigator.mediaDevices #js {:audio #js {:sampleRate 16000}})
+      (.then (fn [stream]
+               (let [r-a (create-recorder stream)
+                     r-b (create-recorder stream)]
+                 (reset! recorders [r-a r-b])
+
+                 ;; Recorder A: 5s chunks, restarts every 5s
+                 (.start r-a)
+                 (js/setInterval #(when (:recording? @App-State) (.stop r-a) (.start r-a)) 5000)
+
+                 ;; Recorder B: 5s chunks, starts after 3s (2s overlap)
+                 (js/setTimeout
+                  #(when (:recording? @App-State)
+                     (.start r-b)
+                     (js/setInterval (fn [] (.stop r-b) (.start r-b)) 5000))
+                  3000))))))
+
+;; --- UI Components (Hiccup) ---
+(defn app-view []
+  (let [{:keys [recording? chunks]} @App-State]
+    [:div {:class "has-text-centered":style {:font-family "sans-serif" :padding "20px"}}
+     [:h1 "Whisper.cpp Live Stream"]
+
+     [:button {:on-click (if recording? stop-streaming! start-streaming!)
+               :style {:background (if recording? "#ff4444" "#44cc44")
+                       :color "white" :padding "10px 20px" :border "none" :border-radius "4px"}}
+      (if recording? "Stop Recording" "Start Recording")]
+
+     [:div {:style {:margin-top "20px" :border "1px solid #ccc" :padding "10px" :min-height "200px"}}
+      [:h3 "Transcriptions (Overlapping Segments):"]
+      (if (empty? chunks)
+        [:p {:style {:color "#888"}} "Silence..."]
+        (for [text chunks]
+          [:p {:key text :style {:border-bottom "1px solid #eee" :padding "5px 0"}} text]))]]))
+
+;; --- Mount ---
+; (render [app-view] (js/document.getElementById "app"))
+(render
+  [app-view]
   (or
     (js/document.getElementById "app")
     (doto (js/document.createElement "div")
